1d11dd6a7e10066284ccd8deb08f1a484f36eef3
Tomoyuki Sahara <sahara@caddr.net>
2023-10-14 19:22:54 +0900

    Initial Commit

---
+++ Shogiban/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shogiban
+//
+//  Created by 佐原具幸 on 2023/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shogiban/ShogibanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShogibanApp.swift
+//  Shogiban
+//
+//  Created by 佐原具幸 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ShogibanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
